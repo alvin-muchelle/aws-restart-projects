efe56a5b8b52166b126c307e18a647c656c2b3bf
Alvin Muchelle <muchellealvin@gmail.com>
2025-03-11 14:30:57 +0300

    update analyze_insulin.sh

--- analyze_insulin.sh
+++ analyze_insulin.sh
@@ -2,7 +2,7 @@
 
 sed '/ORIGIN/d; /\/\//d' preproinsulin-seq.txt. | tr -d '[:digit:] [:space:]' >  preproinsulin-seq-clean.txt
 preproinsulin=$(cat preproinsulin-seq-clean.txt)
-echo "$preproinsulin"
+# echo "$preproinsulin"
 
 grep -o '[a-z]' preproinsulin-seq-clean.txt | tr -d '\n' | cut -c1-24>  isinsulin-seq-clean.txt
 isinsulin=$(cat isinsulin-seq-clean.txt)
